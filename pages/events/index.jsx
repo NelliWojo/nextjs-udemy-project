59017a2e973fbeaf566bc0772c119923b9b70768
NelliWojo <nelli.timorshina@wojo.com>
2023-10-30 11:44:34 +0100

    :sparkles: all events data fetch

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,23 @@
 import EventsList from "@/components/events/EventsList";
 import EventsSearch from "@/components/events/EventsSearch";
-import { getAllEvents } from "@/data/data";
+import { getAllEvents } from "@/utils/api-utils";
 import { useRouter } from "next/router";
 import React, { Fragment } from "react";
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
-  // const events = Array.from(getAllEvents);
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+
+const AllEventsPage = (props) => {
+  const { events } = props;
+
   const router = useRouter();
 
   function findEventsHandler(year, month) {
