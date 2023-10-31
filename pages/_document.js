278791cb7cfac6aa67cd8cb807c8c9aaa8e9e09f
NelliWojo <nelli.timorshina@wojo.com>
2023-10-31 11:22:02 +0100

    :bento: created Document file for additional to Head info

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+const { Document, Html, Head, Main, NextScript } = require("next/document");
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <div id="overlays" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
